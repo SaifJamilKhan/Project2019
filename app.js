62137d7a4c5469637a3137af73cedc7237de7ba9
Saif Khan <sj5khan@uwaterloo.ca>
2018-04-28 02:00:35 -0400

    Added title

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
